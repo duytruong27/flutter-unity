8e12f60fa50e47bbe3f3d81f70069c65d171b54e
Duy Truong <duy@ki-elements.de>
2020-06-22 08:40:44 +0200

    make channel changable

--- flutter_unity_platform_interface/lib/method_channel_flutter_unity.dart
+++ flutter_unity_platform_interface/lib/method_channel_flutter_unity.dart
@@ -3,7 +3,7 @@ import 'flutter_unity_platform_interface.dart';
 
 /// An implementation of [FlutterUnityPlatform] that uses method channels.
 class MethodChannelFlutterUnity extends FlutterUnityPlatform {
-  static const MethodChannel channel =
+  static MethodChannel channel =
       MethodChannel('plugins.flutter.io/flutter_unity');
 
   @override
