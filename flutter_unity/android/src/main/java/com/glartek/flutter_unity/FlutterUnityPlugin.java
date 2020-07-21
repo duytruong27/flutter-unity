78d1f78f5c31982ad3cc052016ce7e90642f1b16
Duy Truong <duy@ki-elements.de>
2020-07-21 10:59:46 +0200

    merge change from master

--- flutter_unity/android/src/main/java/com/glartek/flutter_unity/FlutterUnityPlugin.java
+++ flutter_unity/android/src/main/java/com/glartek/flutter_unity/FlutterUnityPlugin.java
@@ -1,7 +1,9 @@
 package com.glartek.flutter_unity;
 
+import android.app.Activity;
 import android.view.WindowManager;
 
+import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
 import org.json.JSONObject;
@@ -14,12 +16,16 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 
+@Keep
 public class FlutterUnityPlugin implements FlutterPlugin, ActivityAware {
     static List<FlutterUnityView> views = new ArrayList<>();
 
     private FlutterPluginBinding flutterPluginBinding;
+    private int initialActivityRequestedOrientation;
+    private Activity currentActivity;
     private FlutterUnityPlayer player;
 
+    @Keep
     public static void onMessage(String data) {
         Log.d(String.valueOf(FlutterUnityPlugin.class), "onMessage: " + data);
         try {
@@ -52,36 +58,49 @@ public class FlutterUnityPlugin implements FlutterPlugin, ActivityAware {
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         Log.d(String.valueOf(this), "onAttachedToActivity");
-        player = new FlutterUnityPlayer(binding.getActivity());
-        binding.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        onActivity(binding.getActivity());
     }
 
     @Override
     public void onDetachedFromActivityForConfigChanges() {
         Log.d(String.valueOf(this), "onDetachedFromActivityForConfigChanges");
-        player.destroy();
-        player = null;
+        onActivity(null);
     }
 
     @Override
     public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
         Log.d(String.valueOf(this), "onReattachedToActivityForConfigChanges");
-        player = new FlutterUnityPlayer(binding.getActivity());
-        binding.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        onActivity(binding.getActivity());
     }
 
     @Override
     public void onDetachedFromActivity() {
         Log.d(String.valueOf(this), "onDetachedFromActivity");
-        player.destroy();
-        player = null;
+        onActivity(null);
     }
 
     FlutterPluginBinding getFlutterPluginBinding() {
         return flutterPluginBinding;
     }
 
+    void resetScreenOrientation() {
+        currentActivity.setRequestedOrientation(initialActivityRequestedOrientation);
+    }
+
     FlutterUnityPlayer getPlayer() {
         return player;
+    }
+
+    private void onActivity(Activity activity) {
+        if (activity != null) {
+            initialActivityRequestedOrientation = activity.getRequestedOrientation();
+            currentActivity = activity;
+            player = new FlutterUnityPlayer(activity);
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            currentActivity = null;
+            player.destroy();
+            player = null;
+        }
     }
 }
